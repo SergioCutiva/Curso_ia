2736f7483633ffad887ad5597412fd50da5cafea
Sergio_Cutiva <scutiva@unal.edu.co>
2025-08-16 08:17:35 -0500

    kmeans

--- Conceptos_Previos/Mision2/Clientes/kmeans_supermercado.py
+++ Conceptos_Previos/Mision2/Clientes/kmeans_supermercado.py
@@ -4,12 +4,34 @@ from sklearn.cluster import KMeans
 from mpl_toolkits.mplot3d import Axes3D
 
 
-# Cargar datos
+#===========================
+# 1. Carga de datos
+#===========================
 df = pd.read_csv('clientes_supermercado.csv')
 print(df)
 
-# Redondear antes de convertir en int
+#==================================
+# Redondear antes de convertir a int
+#==================================
 
 df['Edad'] = df['Edad'].round().astype(int)
 df['VisitasPorMes']=df['VisitasPorMes'].round().astype(int)
 print(df)
+x = df[['Edad','GastoMensual','VisitasPorMes']]
+
+#=======================================
+# Metodo del codo
+#=======================================
+inertias=[]
+k_range = range(1,10)
+for k in k_range:
+    kmeans=KMeans(n_clusters=k,random_state=42)
+    kmeans.fit(x)
+    inertias.append(kmeans.inertia_)
+
+plt.figure(figsize=(6,4))
+plt.plot(k_range,inertias,marker='o')
+plt.xlabel('Numero de cluster')
+plt.ylabel('Inercia')
+plt.title('Metodo del codo Sergmentación de clientes')
+plt.show()
